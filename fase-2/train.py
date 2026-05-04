b042b58b3ba8adc47902db8ff4f6759d4d6dabfc
anamariagg <80848976+anamariagg@users.noreply.github.com>
2026-05-03 21:05:32 -0500

    Set random_state for RandomForestRegressor

--- fase-2/train.py
+++ fase-2/train.py
@@ -17,7 +17,7 @@ y = data['nota_final']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Modelo
-model = RandomForestRegressor()
+model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
 # Guardar modelo
